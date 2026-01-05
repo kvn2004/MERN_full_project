53b28added7f5999f2c3166bcc3aabe292d86a5f
kvn2004 <knimsaravihanga@gmail.com>
2026-01-06 03:10:21 +0530

    sameSite issue fixed

--- backend/src/utils/generateToken.util.ts
+++ backend/src/utils/generateToken.util.ts
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = ({ res }: any, userId: string) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
 
