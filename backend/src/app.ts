878755aa6ad6b4fcead1e2f3dbf150a2d8105066
kvn2004 <knimsaravihanga@gmail.com>
2026-01-06 00:10:11 +0530

    few bugs fixed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,13 +9,16 @@ import cookieParser from "cookie-parser";
 import "./cron/notification.cron";
 import chatRoutes from "./routes/chat.routes";
 
-
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "",
-  credentials: true
-}));
-app.use(express.json()); // it parses incoming JSON requests and puts the parsed data in req.body 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json()); // it parses incoming JSON requests and puts the parsed data in req.body
 app.use(cookieParser());
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
@@ -23,6 +26,5 @@ app.use("/cycle", cycleRoutes);
 app.use("/partners", partnerRoutes);
 app.use("/notifications", notificationRoutes);
 app.use("/chat", chatRoutes);
-
 
 export default app;
